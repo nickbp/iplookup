edfeed8bd1d0eca70eec211cc5a1aadd5fafc06c
Nick Parker <nick@nickbp.com>
2020-06-06 17:37:37 +1200

    Clean up internal error handling and produce nicer error messages

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 /*
     iplookup - Query STUN service for current public IP address
-    Copyright (C) 2019  Nicholas Parker
+    Copyright (C) 2020  Nicholas Parker
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,17 +18,14 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+use anyhow::{anyhow, bail, Context, Result};
 use bytecodec::{DecodeExt, EncodeExt};
 use rand::Rng;
 use std::env;
-use std::io;
 use std::net::{SocketAddr, ToSocketAddrs};
-use std::result::Result;
 use std::time::Duration;
 use stun_codec::rfc5389::{methods, Attribute};
-use stun_codec::{
-    BrokenMessage, Message, MessageClass, MessageDecoder, MessageEncoder, TransactionId,
-};
+use stun_codec::{Message, MessageClass, MessageDecoder, MessageEncoder, TransactionId};
 use tokio::net::UdpSocket;
 use tokio::time;
 
@@ -40,25 +37,23 @@ fn print_syntax() {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), io::Error> {
+async fn main() -> Result<()> {
     if env::args().len() <= 1 {
         print_syntax();
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "Missing required argument",
-        ));
+        bail!("Missing required argument");
     }
     let endpoint = env::args().nth(1).unwrap();
     if endpoint.starts_with('-') {
         // Probably a commandline argument like '-h'/'--help', avoid parsing as a hostname
         print_syntax();
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "Unrecognized argument",
-        ));
+        bail!("Unrecognized argument");
     }
     // Probably an endpoint, try to resolve it in case it's a hostname
-    let addr = resolve(endpoint)?;
+    let addr = endpoint
+        .to_socket_addrs()
+        .with_context(|| format!("Invalid or unresolvable endpoint: {}", endpoint))?
+        .next()
+        .with_context(|| format!("Missing addresses in endpoint resolution: {}", endpoint))?;
     let local_addr = "0.0.0.0:0".to_socket_addrs()?.next().unwrap();
     let mut conn = UdpSocket::bind(local_addr).await?;
 
@@ -77,22 +72,8 @@ async fn main() -> Result<(), io::Error> {
     }
 }
 
-fn resolve(endpoint: String) -> Result<SocketAddr, io::Error> {
-    match endpoint.to_socket_addrs() {
-        Ok(mut addrs) => Ok(addrs.next().expect("Missing addresses from OK response")),
-        Err(_err) => Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            format!("Invalid or unresolvable UDP endpoint: {}", endpoint),
-        )),
-    }
-}
-
 /// Runs the client: Sends a request and prints the response
-async fn run_client(
-    conn: &mut UdpSocket,
-    dest: &SocketAddr,
-    debug: bool,
-) -> Result<SocketAddr, io::Error> {
+async fn run_client(conn: &mut UdpSocket, dest: &SocketAddr, debug: bool) -> Result<SocketAddr> {
     // Build and send request
     let mut transaction_id_buf = [0u8; 12];
     rand::thread_rng().try_fill(&mut transaction_id_buf)?;
@@ -100,11 +81,11 @@ async fn run_client(
     let message =
         Message::<Attribute>::new(MessageClass::Request, methods::BINDING, transaction_id);
     if debug {
-        eprintln!("Sending: {:?}", &message);
+        eprintln!("Sending: {:#?}", &message);
     }
     let message_bytes = MessageEncoder::new()
         .encode_into_bytes(message)
-        .map_err(std_err_codec)?;
+        .context("Codec error when encoding request")?;
 
     // Wait for response, use arbitrarily large buf that shouldn't realistically be exceeded by UDP
     let mut recvbuf = [0u8; 2048];
@@ -114,22 +95,20 @@ async fn run_client(
     let mut decoder = MessageDecoder::<Attribute>::new();
     let decoded = decoder
         .decode_from_bytes(&recvbuf[..recvsize])
-        .map_err(std_err_codec)?
-        .map_err(std_err_msg)?;
+        .context("Codec error when decoding response")?
+        // Would use another .context() call, but BrokenMessage is incompatible.
+        .map_err(|e| anyhow!("Message error when decoding response: {:?}", e))?;
     if debug {
-        eprintln!("Received ({}b): {:?}", recvsize, decoded);
+        eprintln!("Received ({}b): {:#?}", recvsize, decoded);
     }
 
     // Check that the returned transaction ID matches what we sent
     if transaction_id != decoded.transaction_id() {
-        return Err(io::Error::new(
-            io::ErrorKind::InvalidData,
-            format!(
-                "Returned transaction id {:?} doesn't match sent {:?}",
-                decoded.transaction_id(),
-                transaction_id
-            ),
-        ));
+        bail!(
+            "Returned transaction id {:?} doesn't match sent {:?}",
+            decoded.transaction_id(),
+            transaction_id
+        );
     }
 
     let result = decoded
@@ -147,13 +126,7 @@ async fn run_client(
         })
         .nth(0);
 
-    match result {
-        Some(addr) => Ok(addr),
-        None => Err(io::Error::new(
-            io::ErrorKind::NotFound,
-            format!("No address attribute found in response: {:?}", decoded),
-        )),
-    }
+    result.with_context(|| format!("No address attribute found in response: {:?}", decoded))
 }
 
 async fn recv_exponential_backoff(
@@ -161,7 +134,7 @@ async fn recv_exponential_backoff(
     dest: &SocketAddr,
     sendbuf: &Vec<u8>,
     mut recvbuf: &mut [u8],
-) -> Result<usize, io::Error> {
+) -> Result<usize> {
     // Receive timeout durations: 1s, 2s, 4s, 8s, 16s (total wait: 31s)
     const RETRIES: u32 = 5;
     for timeout_exponent in 0..RETRIES {
@@ -189,7 +162,7 @@ async fn recv_exponential_backoff(
                 );
             }
             // A different error occurred, give up
-            Ok(Err(e)) => return Err(e),
+            Ok(Err(e)) => Err(e).context("Failed to receive STUN response")?,
             // Timeout occurred, try again (or exit loop)
             Err(_) => {
                 if timeout_exponent + 1 == RETRIES {
@@ -200,24 +173,5 @@ async fn recv_exponential_backoff(
             }
         }
     }
-    Err(io::Error::new(
-        io::ErrorKind::TimedOut,
-        format!("Timed out waiting for response from {:?}", dest),
-    ))
-}
-
-// Clean up after the library author's decision to obnoxiously reinvent the wheel with their own error types.
-fn std_err_codec(from: bytecodec::Error) -> io::Error {
-    io::Error::new(
-        io::ErrorKind::InvalidData,
-        format!("Codec error: {:?}", from),
-    )
-}
-
-// Another day, another reinvented wheel
-fn std_err_msg(from: BrokenMessage) -> io::Error {
-    io::Error::new(
-        io::ErrorKind::InvalidData,
-        format!("Message error: {:?}", from),
-    )
+    bail!("Timed out waiting for response from {:?}", dest)
 }
