5516db4ffade0f7849295619990f3089175b54e0
Nick Parker <nick@nickbp.com>
2021-01-12 15:56:59 +1300

    Switch to plain buildx in build script

--- docker-build.sh
+++ docker-build.sh
@@ -10,7 +10,7 @@ PROJ_NAME=$(basename $SCRIPT_DIR)
 
 DOCKER_REGISTRY=${DOCKER_REGISTRY:=registry.gitlab.com}
 DOCKER_REPOSITORY=${DOCKER_REPOSITORY:="nickbp/k8s/${PROJ_NAME}:"}
-DOCKER_BUILD=${DOCKER_BUILD:="sudo img build --platform linux/amd64,linux/arm64"}
+DOCKER_BUILD=${DOCKER_BUILD:="docker buildx build --platform linux/amd64,linux/arm64"}
 
 # Get 7-character commit SHA (note: doesn't detect dirty commits)
 COMMIT_SHA=$(git rev-parse HEAD | cut -b 1-7)
